a9cc90cc11ada4da0ca3fedce71487263931b62c
abraaowendel <abraao.dev@gmail.com>
2022-03-09 19:05:14 -0300

    Login

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center">
+  <br>DevMax
+</h1>
+
+<h4 align="center">
+     O que é DevMax?<br>
+     DevMax é um serviço de stream com várias séries, filmes e desenhos.
+</h4>
+
+<h4 align="center"><a href="https://abraaowendel.github.io/DevMax/" target="_blank">Clique para visitar o projeto</a></h4>
+
+## 💼 Tecnologias utilizadas:
+
+Para o desenvolvimento deste projeto usei as seguintes tecnologias:
+
+- HTML5;
+- CSS3;
+- JavaScript;
+
+---
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-const items = document.querySelectorAll('.modal-item');
 
-items.forEach((a) =>{
-    a.addEventListener('click', (e) =>{
-      e.currentTarget.classList.toggle('active')
-    })
+const show = document.getElementById('senha')
+const label = document.getElementById('label-password')
+
+const labelfloat = () => show.value.length > 0? label.classList.add('active'):label.classList.remove('active')
+document.body.addEventListener('click', labelfloat)
+
+
+document.querySelector('.show-password').addEventListener('click', () =>{
+    const show = document.getElementById('senha')
+    const actual = show.getAttribute("type");
+    if(actual === "password"){
+        document.querySelector('.show-password').innerHTML = 'OCULTAR';
+        show.setAttribute("type", "text")
+    }
+    else{
+        document.querySelector('.show-password').innerHTML = 'MOSTRAR';
+        show.setAttribute("type", "password")
+    }
 })
-
 
